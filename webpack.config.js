3a0d63d6bb515c6ca09a5758736814a8b73fa85f
lotostoii <lotostoii@gmail.com>
2020-11-18 17:37:33 +1000

    in work

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 let conf = {
-  entry: './src/main.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'main.js',
